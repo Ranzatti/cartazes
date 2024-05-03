fb3735f395ba4bb9533ab165bc1e5df9727e98be
ranzatti <ranzatti@sonner.com.br>
2024-05-03 16:25:50 -0300

    atualizacao

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://snrmyrdk:WdWoPvt6qtVm9uq8V93rSYGKkf3WW-nt@tuffi.db.elephantsql.com/snrmyrdk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://socorrpm:SICSADM@localhost:5432/socorrpm'
 
 db = SQLAlchemy(app)
 
